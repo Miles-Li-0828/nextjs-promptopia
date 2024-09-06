74b9130c187b1e9c2f52aed64dec1a6178921a13
Miles Li <128461924+Miles-Li-0828@users.noreply.github.com>
2024-09-06 15:57:44 +1000

    change

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                satoshi: ['Satoshi', 'sans-serif'],
+                inter: ['Inter', 'sans-serif'],
+            },
+            colors: {
+                'primary-orange': '#FF5722',
+            },
+            boxShadow: {
+                'custom': 'inset 10px -50px 94px 0 rgba(199, 199, 199, 0.2)',
+            },
+            backgroundImage: {
+                'glassmorphism': 'radial-gradient(circle, rgba(255, 255, 255, 0.2) 0%, rgba(199, 199, 199, 0.2) 100%)',
+            },
+        },
+    },
+    plugins: [],
+}
